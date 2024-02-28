a1f1b95df0c3d6b8bc2ef7b28535af85a9fc613a
Luyys Henrique <121971847+LuyysHenriqueKuromi@users.noreply.github.com>
2024-02-27 21:37:52 -0300

    Tr

---
+++ TrocaDeValores.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+public class TrocaDeValores {
+    public static void main(Strang[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Difite o primeiro valor: ");
+        float num1 = s.nextFloat();
+        
+        System.out.print(("Digite o segundo valor: "));
+        float num2 = s
+    }
+    
+}
