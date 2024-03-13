f3f5939228e52e31e519747efce3c2811ed44be4
Luyys Henrique <121971847+LuyysHenriqueKuromi@users.noreply.github.com>
2024-03-13 20:08:23 -0300

    PlanetapesoIf_Else.java
    semi_completo

--- PlanetapesoIf_Else.java
+++ PlanetapesoIf_Else.java
@@ -4,20 +4,23 @@ public class PlanetapesoIf_Else {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.print("Digite para onde você queira ir: [M]Marte/[L]Lua ");
-        String lugar = s.nextLine();
-
         System.out.print("Digite o seu peso: ");
         float peso = s.nextFloat();
 
+        System.out.print("Para qual corpo celeste você quer ir? Lua[L]/Marte[M]: ");
+        String lugar = s.nextLine();
+
         float peso_lug = 0
 
-        if (lugar.equals("M")) {
+        if (lugar.equals("L")) {
             peso_lug = (peso / 9.81) * 1.62;
-        }else if (lugar.equals("F")) {
+        }else if (lugar.equals("M")) {
             peso_lug = (peso / 9.81) * 3.71;
         }else {
             System.out.print("ERRO");
         }
+
+        System.out.printf("O seu peso que é %.2f kg na terre, vira %.2f kg para onde você viajou",
+        peso, peso_lugar);
     }
 }
