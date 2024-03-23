623d0c1bc4f8b8244b3bfb7f2cf50f56e7ccc11f
Luyys Henrique <121971847+LuyysHenriqueKuromi@users.noreply.github.com>
2024-03-22 23:56:48 -0300

    DistanciaCartes.java
    reorganização dos tipos de variaveis

--- DistanciaCartes.java
+++ DistanciaCartes.java
@@ -3,28 +3,31 @@ import java.util.Scanner;
 public class DistanciaCartes {
     public static void main (String[] args) {
         Scanner s = new Scanner(System.in);
+        float a1x, a1y, b2x, b2y, valor1, valor2;
+        double preresult, result;
+
         System.out.print("Digite o valor de X no ponto A: ");
-        float a1x = s.nextFloat();
+        a1x = s.nextFloat();
 
         System.out.print("Digite o valor de Y no ponto A: ");
-        float a1y = s.nextFloat();
+        a1y = s.nextFloat();
 
         System.out.print("Digite o valor de X no ponto B: ");
-        float b2x = s.nextFloat();
+        b2x = s.nextFloat();
 
         System.out.print("Digite o valor de Y no ponto B: ");
-        float b2y = s.nextFloat();
+        b2y = s.nextFloat();
 
-        float valor1 = (b2x - a1x);
+        valor1 = (b2x - a1x);
 
-        float valor2 = (b2y - a1y);
+        valor2 = (b2y - a1y);
         
-        double preresult = Math.pow(valor1, 2) +  Math.pow(valor2, 2);
+        preresult = Math.pow(valor1, 2) +  Math.pow(valor2, 2);
 
-        double result = Math.sqrt(preresult);
+        result = Math.sqrt(preresult);
 
         System.out.printf("A distância dos pontos A e B, é %2.1f"
                 , result);
 
     }
-}    
+}
