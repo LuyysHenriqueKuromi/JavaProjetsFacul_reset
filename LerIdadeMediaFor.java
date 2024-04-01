81e4b9e63c17ef72ef82ce3c2d57143b175440eb
Luyys Henrique <121971847+LuyysHenriqueKuromi@users.noreply.github.com>
2024-04-01 20:50:09 -0300

    LerIdadeMediaFor.java

--- LerIdadeMediaFor.java
+++ LerIdadeMediaFor.java
@@ -3,25 +3,20 @@ import java.util.Scanner;
 public class LerIdadeMediaFor {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        double idade, soma, media, quat_pessoa;
+        double idade, soma, media;
 
         soma = 0;
-        quat_pessoa = 0;
 
-        for (int i = 1; i < 5; i++) {
-            System.out.printf("Digite a idade da %d° pessoa: ",
-            i);
-            
-            idade = s.nextInt();
+        for (int i = 1; i < 6; i++) {
+            System.out.printf("Digite a idade da %d° pessoa: ", i);
+            idade = s.nextDouble();
 
-            soma = soma + idade;
-
-            quat_pessoa = quat_pessoa + 1;
+            soma += idade;
         }
 
-        media = soma / quat_pessoa;
+        media = soma / 5;
 
-        System.out.printf("A media das idades é: %d",
+        System.out.printf("A media das idades é %1.2f",
         media);
     }
 }
