b1a75e7aae89b28b21e7b9cc6ba8de1fda4f087e
Luyys Henrique <121971847+LuyysHenriqueKuromi@users.noreply.github.com>
2024-04-02 20:22:28 -0300

    While
    aprendendo a usar o While no Java

---
+++ AuladWhile.java
@@ -0,0 +1,10 @@
+public class AuladWhile {
+    public static void main(String[] args) {
+        int cont = 0;
+
+        while (cont < 10) {
+            System.out.println(cont + "- Eu amo programar!");
+            cont+=2;
+        }
+    }
+}
