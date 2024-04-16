33906263c55d46188c106a401e2c2dcd9eb4fbc5
Luyys Henrique <121971847+LuyysHenriqueKuromi@users.noreply.github.com>
2024-04-16 20:21:57 -0300

    Salario.java

--- Salario.java
+++ Salario.java
@@ -4,11 +4,12 @@ public class Salario {
     public static void main(String[] args) {
         
         Scanner s = new Scanner(System.in);
+        double salar_min, salar_total, aum_salar;
         System.out.print("Quantos salerios você recebe? ");
         float salar_quant = s.nextFloat();
-        double salar_min = 1412.00;
-        double salar_total = salar_quant * salar_min;
-        double aum_salar = salar_total * 1.10;
+        salar_min = 1412.00;
+        salar_total = salar_quant * salar_min;
+        aum_salar = salar_total * 1.10;
         
         System.out.printf("O seu salário teve um aumento de 10 porcento, de R$%2.2f, foi para R$%2.2f"
                 ,salar_total, aum_salar);
